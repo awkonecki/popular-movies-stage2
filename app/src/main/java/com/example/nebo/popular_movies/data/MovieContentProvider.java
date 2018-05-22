25df155bcefe5453d809858a6240bea334d93c02
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-05-22 12:43:44 -0700

    Insertion Implementaiton.

--- app/src/main/java/com/example/nebo/popular_movies/data/MovieContentProvider.java
+++ app/src/main/java/com/example/nebo/popular_movies/data/MovieContentProvider.java
@@ -4,13 +4,15 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 public class MovieContentProvider extends ContentProvider {
 
-    private MovieDBHelper movieDBHelper = null;
+    private static MovieDBHelper mMovieDBHelper = null;
     private static final UriMatcher sUriMatcher = MovieContentProvider.buildUriMatcher();
 
     // Building URI matcher to define the set of Uri expressions that are acceptable with the
@@ -35,14 +37,8 @@ public class MovieContentProvider extends ContentProvider {
     @Override
     public boolean onCreate() {
         // Setup the helper for the content provider to interact with.
-        this.movieDBHelper = new MovieDBHelper(getContext());
+        this.mMovieDBHelper = new MovieDBHelper(getContext());
         return true;
-    }
-
-    @Nullable
-    @Override
-    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
-        return null;
     }
 
     @Nullable
@@ -53,17 +49,86 @@ public class MovieContentProvider extends ContentProvider {
 
     @Nullable
     @Override
-    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        int match = MovieContentProvider.sUriMatcher.match(uri);
+
+        switch(match) {
+            case MovieContentProvider.MOVIES:
+                break;
+            case MovieContentProvider.MOVIES_WITH_ID:
+                break;
+            default:
+                break;
+        }
+
         return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        // Obtain the descriptor reference to the SQLite databse.
+        final SQLiteDatabase database = MovieContentProvider.mMovieDBHelper.getWritableDatabase();
+        int match = MovieContentProvider.sUriMatcher.match(uri);
+
+        Uri resultUri = null;
+
+        switch(match) {
+            case MovieContentProvider.MOVIES:
+                long id = database.insert(MovieContract.PATH_FAVORITE_MOVIES,
+                        null,
+                        values);
+
+                if (id == -1) {
+                    throw new SQLException(
+                            "Could not insert values into the Favorite Movies table."
+                    );
+                }
+                else {
+                    resultUri = Uri.withAppendedPath(MovieContract.MovieEntry.CONTENT_URI,
+                            Long.toString(id));
+                }
+
+                break;
+            default:
+                throw new java.lang.UnsupportedOperationException("Un-supported Uri operation.");
+        }
+
+        // Need to notify the resolver.
+        getContext().getContentResolver().notifyChange(resultUri, null);
+
+        return resultUri;
     }
 
     @Override
     public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int match = MovieContentProvider.sUriMatcher.match(uri);
+
+        switch(match) {
+            case MovieContentProvider.MOVIES:
+                break;
+            case MovieContentProvider.MOVIES_WITH_ID:
+                break;
+            default:
+                break;
+        }
+
         return 0;
     }
 
     @Override
     public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int match = MovieContentProvider.sUriMatcher.match(uri);
+
+        switch(match) {
+            case MovieContentProvider.MOVIES:
+                break;
+            case MovieContentProvider.MOVIES_WITH_ID:
+                break;
+            default:
+                break;
+        }
+
         return 0;
     }
 }
