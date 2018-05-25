2e74cb19db33195c359aacd9eb4c16db18f3b3e9
awkonecki.developer@gmail.com <awkonecki.developer@gmail.com>
2018-05-25 15:23:07 -0700

    MovieDetail UI Population
    Fixed UI bugs for the DataBinding
    - setting the contentView for the databinding means that only one can be referenced for a given view.
    - can not set the background with the parent layout within the same activity.  Will need to driectly reference the image view if want to still populate it.

--- app/src/main/java/com/example/nebo/popular_movies/MovieDetailActivity.java
+++ app/src/main/java/com/example/nebo/popular_movies/MovieDetailActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 
 import com.example.nebo.popular_movies.databinding.MovieDetailContentBinding;
 import com.example.nebo.popular_movies.databinding.MovieDetailBinding;
@@ -24,6 +25,10 @@ public class MovieDetailActivity extends AppCompatActivity {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_detail);
+        this.mDetailBinding = DataBindingUtil.setContentView(this,
+                R.layout.movie_detail_content);
+        // this.mMovieBinding = DataBindingUtil.setContentView(this,
+        //        R.layout.movie_detail);
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -49,6 +54,7 @@ public class MovieDetailActivity extends AppCompatActivity {
         }
 
         this.populateUI(movie);
+        Log.d("Movie Contents", movie.toString());
     }
 
     @Override
@@ -58,25 +64,24 @@ public class MovieDetailActivity extends AppCompatActivity {
     }
 
     private void populateUI(Movie movie) {
-        MovieDetailContentBinding detailBinding = DataBindingUtil.getBinding(this.getCurrentFocus());
-        MovieDetailBinding movieBinding = DataBindingUtil.getBinding(this.getCurrentFocus());
         String title = null;
+
+        if (this.mDetailBinding == null) {
+            Log.d("Populate UI", "null bindings : ");
+            return;
+        }
 
         if (movie != null) {
             Picasso.get().load(movie.getPosterPath()).error(R.drawable.image_placeholder).
-                    into(detailBinding.ivMoivePosterDetail);
-            Picasso.get().load(movie.getBackdropPath()).error(R.drawable.image_placeholder).
-                    into(movieBinding.ivBackgroundDetail);
+                    into(this.mDetailBinding.ivMoivePosterDetail);
+            // Picasso.get().load(movie.getBackdropPath()).error(R.drawable.image_placeholder).
+            //        into(this.mMovieBinding.ivBackgroundDetail);
 
-            if (movie.getTitle() == null) {
-                title = getString(R.string.default_title);
-            }
-
-            this.setTitle(title);
-            detailBinding.tvLabelSummary.setText(movie.getOverview());
-            detailBinding.tvLabelReleaseDate.setText(movie.getReleaseDate());
-            detailBinding.tvMovieDetailRating.setText(Double.toString(movie.getVote()));
-            detailBinding.tvLabelTitle.setText(movie.getTitle());
+            this.setTitle(movie.getTitle());
+            this.mDetailBinding.tvMovieDescription.setText(movie.getOverview());
+            this.mDetailBinding.tvMovieDetailReleaseDate.setText(movie.getReleaseDate());
+            this.mDetailBinding.tvMovieDetailRating.setText(Double.toString(movie.getVote()));
+            this.mDetailBinding.tvMovieDetailTitle.setText(movie.getTitle());
         }
 
         // 1. Creation of the layout managers for the reviews and trailers.
@@ -96,16 +101,15 @@ public class MovieDetailActivity extends AppCompatActivity {
                 R.layout.movie_trailer_item);
 
         // 3. Adding of the layout manager to the recycler views.
-        detailBinding.rvMovieDetailReviews.setLayoutManager(reviewLayoutManager);
-        detailBinding.rvMovieDetailTrailers.setLayoutManager(trailerLayoutManager);
+        this.mDetailBinding.rvMovieDetailReviews.setLayoutManager(reviewLayoutManager);
+        this.mDetailBinding.rvMovieDetailTrailers.setLayoutManager(trailerLayoutManager);
 
         // 4. Adding of the adapters to the recycler views.
-        detailBinding.rvMovieDetailReviews.setAdapter(reviewAdapter);
-        detailBinding.rvMovieDetailTrailers.setAdapter(trailerAdapter);
+        this.mDetailBinding.rvMovieDetailReviews.setAdapter(reviewAdapter);
+        this.mDetailBinding.rvMovieDetailTrailers.setAdapter(trailerAdapter);
 
         // 5. Set settings for recycler viewer.
-        detailBinding.rvMovieDetailReviews.setHasFixedSize(true);
-        detailBinding.rvMovieDetailTrailers.setHasFixedSize(true);
-
+        this.mDetailBinding.rvMovieDetailReviews.setHasFixedSize(true);
+        this.mDetailBinding.rvMovieDetailTrailers.setHasFixedSize(true);
     }
 }
